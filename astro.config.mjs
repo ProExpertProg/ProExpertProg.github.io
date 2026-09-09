5eab24e9bfb59f270e5bab47ca98551760b22988
Luka Govedic <luka.govedic@gmail.com>
2026-09-08 20:42:20 -0400

    Change domain name, format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // If you buy a domain later, change this and add a public/CNAME file.
-  site: 'https://proexpertprog.github.io',
+  site: 'lukagovedic.com',
 });
